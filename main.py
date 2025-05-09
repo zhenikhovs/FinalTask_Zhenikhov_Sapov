bbdaa978f58dda00e1b40b65d5d3f9f2c48d8622
zhenikhovs <zhenihov01@mail.ru>
2025-05-10 04:18:22 +0500

    Произведено слияние веток master и Znehikhov после мерджа в master ветки Shilov. Исправления после review

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
 from fastapi import FastAPI
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-from transformers import pipeline
+from Summarizer import Summarizer
+from SentimentAnalyzer import SentimentAnalyzer
 
 app = FastAPI()
-class SentimentAnalyzer:
-    def __init__(self):
-        self.classifier = pipeline("sentiment-analysis")
 
-    def analyze(self, text: str):
-        return self.classifier(text)
 sentiment_analyzer = SentimentAnalyzer()
-
-
-class Summarizer:
-    def __init__(self, model_name: str):
-        self.tokenizer = T5Tokenizer.from_pretrained(model_name)
-        self.model = T5ForConditionalGeneration.from_pretrained(model_name).eval()
-
-    def summarize(self, text: str) -> str:
-        if not text:
-            return ''
-        input_ids = self.tokenizer(text, return_tensors='pt').input_ids
-        outputs = self.model.generate(input_ids)
-        summary = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return summary
-
 summarizer = Summarizer('d0rj/rut5-base-summ')
 
 @app.get("/")
 async def root():
-    return {"lab-6": "Для получения краткого содержания текста перейдите по ссылке /get_summary/ и передайте в параметр text ваш полный текст."}
+    return {
+        "health": "Для работоспособности проекта перейдите по ссылке /health/. Если статус success - все работает прекрасно.",
+        "get_summary": "Для получения краткого содержания текста перейдите по ссылке /get_summary/ и передайте в параметр text ваш полный текст.",
+        "get_sentiment": "Для получения тональности текста перейдите по ссылке /get_sentiment/ и передайте в параметр text ваш полный текст.",
+        "get_summary_sentiment": "Для получения краткого содержания текста и тональности краткого содержания перейдите по ссылке /get_summary_sentiment/ и передайте в параметр text ваш полный текст."
+    }
 
 @app.get("/get_summary/")
 async def get_summary(text: str = ''):
+    if not (text):
+         return {
+                "status" : "error",
+                "error_message": "Текст отсутствует. Введите текст."
+            }
     sum_text = summarizer.summarize(text)
     if not sum_text:
-        return {"error": "Слишком короткий текст. Получение краткого содержания невозможно."}
-    return {"summarized_text": sum_text}
+        return {
+            "status" : "error",
+            "error_message": "Слишком короткий текст. Получение краткого содержания невозможно."
+        }
+    return {
+        "status" : "success",
+        "summarized_text": sum_text
+    }
 
 @app.get("/health")
 async def health():
@@ -44,18 +40,37 @@ async def health():
     try:
         summary = summarizer.summarize(test_text)
         if summary:
-            return {"status": "ok"}
+            return {
+                "status": "success"
+            }
         else:
-            return {"status": "not ok", "error": "Суммаризация не работает (пустой ответ)"}
+            return {
+                "status": "error",
+                "error_message": "Суммаризация не работает (пустой ответ)"
+            }
     except Exception as e:
-        return {"status": "not ok", "error": str(e)}
+        return {
+            "status": "error",
+            "error_message": str(e)
+        }
 
+@app.get("/get_sentiment/")
+async def get_sentiment(text: str = ''):
+    sentiment = sentiment_analyzer.analyze(sum_text)
+    return {
+            "status" : "success",
+            "sentiment": sentiment
+        }
 
 @app.get("/get_summary_sentiment/")
 async def get_summary_sentiment(text: str = ''):
     return summarize_and_analyze(text)
 
 def summarize_and_analyze(text: str):
-    sum_text = get_summ_text(text)
+    sum_text = summarizer.summarize(text)
     sentiment = sentiment_analyzer.analyze(sum_text)
-    return {"summarized_text": sum_text, "sentiment": sentiment}
+    return {
+        "status" : "success",
+        "summarized_text": sum_text,
+        "sentiment": sentiment
+    }
